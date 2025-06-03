10b60094bad0ce579fff6f66a5e445a9cd9f5a30
Josh Leslie <hello@joshmleslie.com>
2025-06-03 11:57:24 -0400

    update messagelog to generic / fixed length

--- src/types/midi.rs
+++ src/types/midi.rs
@@ -1,3 +1,7 @@
+// see MIDI_BYTES.md
 pub type MessageData = [u8; 3];
+// time-stamped data
 pub type Message = (u64, MessageData);
-pub type MessageLog = Vec<Message>;
+pub struct MessageLog<const L: usize> {
+	pub data: [(u64, [u8; 3]); L],
+}
