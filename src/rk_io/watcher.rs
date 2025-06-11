b93d173160974023d40048f9df9d7cbbdd21c33d
Josh Leslie <hello@joshmleslie.com>
2025-06-11 17:53:50 -0400

    update watcher to return a batch_rx

--- src/rk_io/watcher.rs
+++ src/rk_io/watcher.rs
@@ -1,18 +1,24 @@
 use std::env;
-use std::sync::mpsc::channel;
+use std::sync::mpsc::{Receiver, Sender, channel};
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::{Duration, Instant};
 // ---
 use crate::types;
 
-pub fn spawn_watcher() -> std::sync::mpsc::Sender<types::midi::Message> {
+pub fn spawn_watcher() -> (
+    Sender<types::midi::Message>,
+    Receiver<Vec<types::midi::Message>>,
+) {
     let threshold_micro_sec = env::var("THRESHOLD_MICRO_SEC")
         .unwrap()
         .parse::<u64>()
         .unwrap();
 
     let (tx, rx) = channel::<types::midi::Message>();
+    // Output channel for batched MIDI messages (Observable stream)
+    let (batch_tx, batch_rx) = channel::<Vec<types::midi::Message>>();
+
     let batch = Arc::new(Mutex::new(Vec::new()));
     let batch_clone = Arc::clone(&batch);
 
@@ -33,6 +39,14 @@ pub fn spawn_watcher() -> std::sync::mpsc::Sender<types::midi::Message> {
                     for (t, msg) in b.iter() {
                         println!("  {:.3}: {:?}", t, msg);
                     }
+
+                    let batch_to_send = b.clone();
+                    let res = batch_tx.send(batch_to_send);
+                    if res.is_err() {
+                        // Receiver dropped, exit thread
+                        break;
+                    }
+
                     println!("--");
                     b.clear();
                 }
@@ -42,5 +56,5 @@ pub fn spawn_watcher() -> std::sync::mpsc::Sender<types::midi::Message> {
         }
     });
 
-    return tx;
+    return (tx, batch_rx);
 }
